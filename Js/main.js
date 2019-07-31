750b922843c7c1415578fd7423c15b31660cb1b0
Ryan Graham <ryangrahamwpb@gmail.com>
2019-07-31 13:52:32 -0400

    amend functionality and bring in jquery cdn.

--- Js/main.js
+++ Js/main.js
@@ -1,13 +1,22 @@
-var lines = document.getElementsByClassName('lines')[0];
-var sideBar = document.getElementsByClassName('side-bar')[0];
-var main = document.getElementsByTagName('main')[0];
 
-lines.onclick = function() {
- sideBar.style.left = "0";
- sideBar.classList.add("toggled");
-};
 
-main.onclick = function() {
- sideBar.style.left = "-270px";
- sideBar.classList.remove("toggled");
-};
+$('.lines').on('click', function() {
+  $('.side-bar').css('left', '0');
+  $('.side-bar').addClass('toggled');
+});
+
+
+$('main').on('click', function() {
+  $('.side-bar').css('left', '-270px');
+  $('.side-bar').removeClass('toggled');
+});
+
+$(window).on('scroll', function() {
+    if($(this).scrollTop() > 10) {
+      $('nav').css('background', '#fff');
+      $('.nav-link, .logo-h3').css('color', '#FE5151');
+    } else if ($(this).scrollTop() < 10) {
+      $('nav').css('background', 'transparent');
+      $('.nav-link, .logo-h3').css('color', '#fff');
+    }
+});
