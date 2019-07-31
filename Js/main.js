aa3a64e2dc4b1f60385422fb9136524cd6694ddd
Ryan Graham <ryangrahamwpb@gmail.com>
2019-07-31 10:21:14 -0400

    Style side-bar and add class name.

--- Js/main.js
+++ Js/main.js
@@ -4,6 +4,7 @@ var main = document.getElementsByTagName('main')[0];
 
 lines.onclick = function() {
  sideBar.style.left = "0";
+ sideBar.classList.add("toggled");
 };
 
 main.onclick = function() {
