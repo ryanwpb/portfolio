89e8179991166085528af4d9470813309130d457
Ryan Graham <ryangrahamwpb@gmail.com>
2019-07-31 10:16:54 -0400

    Add sidebar open and close functionality.

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,11 @@
+var lines = document.getElementsByClassName('lines')[0];
+var sideBar = document.getElementsByClassName('side-bar')[0];
+var main = document.getElementsByTagName('main')[0];
 
+lines.onclick = function() {
+ sideBar.style.left = "0";
+};
+
+main.onclick = function() {
+ sideBar.style.left = "-270px";
+};
