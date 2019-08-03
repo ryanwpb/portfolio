2ab9dbd3fdfd8823154905195c3d31447c3f7bbe
Ryan Graham <ryangrahamwpb@gmail.com>
2019-08-03 08:49:04 -0400

    Add animated scroll jquery.

--- Js/main.js
+++ Js/main.js
@@ -1,5 +1,5 @@
 
-
+// Side nav toggel.
 $('.lines').on('click', function() {
   $('.side-bar').css('left', '0');
   $('.side-bar').addClass('toggled');
@@ -11,6 +11,8 @@ $('main').on('click', function() {
   $('.side-bar').removeClass('toggled');
 });
 
+
+// Here we change the nav background and a tag color.
 $(window).on('scroll', function() {
     if($(this).scrollTop() > 10) {
       $('nav').css('background', '#fff');
@@ -23,4 +25,12 @@ $(window).on('scroll', function() {
       $('.nav-link, .logo-h3').css('color', '#fff');
       $('.line').css('background', '#fff');
     }
+});
+
+//Animate the scrolling on nav a click
+$('.nav-link, .mobile-link').click(function() {
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 500);
+  return false;
 });
